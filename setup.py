e7303b9a76d49c44a4be8151858020b3a3895ed0
Tom Ritchford <tom@swirly.com>
2019-04-01 12:50:39 +0200

    Update setup.py after move to rec

--- setup.py
+++ setup.py
@@ -14,16 +14,17 @@ _classifiers = [
 if __name__ == '__main__':
     from setuptools import setup
 
-    setup(name='import_all',
-          version=import_all.__version__,
-          author='Tom Ritchford',
-          author_email='tom@swirly.com',
-          url='https://github.com/timedata-org/import_all',
-          tests_require=['pytest'],
-          py_modules=['import_all'],
-          description='Try to import all modules below a given root',
-          long_description=open('README.rst').read(),
-          license='MIT',
-          classifiers=_classifiers,
-          keywords=['testing', 'importing'],
-          )
+    setup(
+        name='import_all',
+        version=import_all.__version__,
+        author='Tom Ritchford',
+        author_email='tom@swirly.com',
+        url='https://github.com/rec/import_all',
+        tests_require=['pytest'],
+        py_modules=['import_all'],
+        description='Try to import all modules below a given root',
+        long_description=open('README.rst').read(),
+        license='MIT',
+        classifiers=_classifiers,
+        keywords=['testing', 'importing'],
+    )
