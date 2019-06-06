5956e12b55d9393d7d20152040e1d355362f243a
Tom Ritchford <tom@swirly.com>
2019-06-06 15:38:55 +0200

    Allow / as a separator, and other improvements

--- import_all.py
+++ import_all.py
@@ -17,29 +17,19 @@ __version__ = '0.9.2'
     Otherwise prints nothing and suceeds.
 """
 
-ENV_PREFIX = '_IMPORT_ALL_'
-ENV_SEPARATOR = ':'
-
-""" ENV_PREFIX is used when setting test attributes using environment
-    variables.  This is convenient for temporarily turning features on or
-    off while debugging.
-
-    For example, to turn off catch exceptions, set the environment variable
-    _IMPORT_ALL_CATCH_EXCEPTIONS=False
-
-    To set a boolean test attribute, use a string starting with t or T for
-    True, or a string starting with f or F for False; any other string gives
-    an error.
-
-    To set a test attribute that's a list of strings, separate those strings
-    with a colon (ENV_SEPARATOR) - for example:
-
-    _IMPORT_ALL_EXCLUDE=my_project.broken:my_project.exper
-"""
-
 
 class TestCase(unittest.TestCase):
-    """ Import every Python module or file and fail on errors or warnings
+    """ Import every Python module or file and fail on errors or warnings.
+
+        You can customize its behavior by setting one of the following test
+        attributes in one of two ways - you can permanently override that test
+        variable in your own test class, or you can set an environment variable
+        beginning with _IMPORT_ALL to temnporarily override the value.
+
+        For example, to turn on catching exceptions, either set
+        CATCH_EXCEPTIONS = True in the definition of your TestCase, or
+        set the environment variable _IMPORT_ALL_CATCH_EXCEPTIONS=True
+        before running the tests.
     """
 
     ALL_SUBDIRECTORIES = False
@@ -47,6 +37,56 @@ class TestCase(unittest.TestCase):
 
         If False, stop searching with subdirectories that do not contain
         an __init__.py file.
+
+        By default, the test attempts to import every Python module and file
+        reachable from its Python root directory.  This means ``import_all``
+        does not load .py files in subdirectories which contain .py files but
+        not a __init__.py file.
+
+        This turns out to be what you want most of the time, but if you want
+        import absolutely everything, set the ALL_SUBDIRECTORIES attribute to
+        be True.  If you want to import more specically, you can use the test
+        attributes EXCLUDE, INCLUDE or PROJECT_PATHS.
+    """
+
+    CATCH_EXCEPTIONS = False
+    """  If CATCH_EXCEPTIONS is False, the first exception will stop the test
+         entirely and print a stack trace
+
+         If True, all exceptions will be caught and reported on at the end.
+         This is most useful when adding this to a new codebase with a lot of
+         import problems.
+    """
+
+    EXCLUDE = None
+    """ A list or tuple of regular expressions, or None.
+
+        If non-empty, modules whose name matches any of these regular
+        expressions will not be imported.
+
+        For convenience, since the character '.' is a wildcard in regular
+        expressions, the '/' character can be used in its place, so these
+        two attribute assignments are the same:
+
+        EXCLUDE = 'foo\\.bar\\.baz:bing\\.bang'
+        EXCLUDE = 'foo/bar/baz:bing/bang'
+    """
+
+    EXPECTED_TO_FAIL = ()
+    """ A list of specific module names that are expected to fail.
+
+        This differs from EXCLUDE because modules which match EXCLUDE aren't
+        imported at all, but the modules in EXPECTED_TO_FAIL must exist, are
+        imported, and then must fail when imported.
+        """
+
+    INCLUDE = None
+    """ A list or tuple of regular expressions, or None.
+
+        If non-empty, only modules whose full pathname matches one of these
+        regular expressions will be imported.
+
+        Just like in the EXCLUDE attribute, '/' can be used instead of '.'
     """
 
     PROJECT_PATHS = None
@@ -56,51 +96,33 @@ class TestCase(unittest.TestCase):
         that contains the definition of the class.
     """
 
-    WARNINGS_ACTION = 'error'
-    """ What to do if a Python warning occurs.
-
-        `warnings.simplefilter` is be set to this value while testing: see
-        https://docs.python.org/3/library/warnings.html#the-warnings-filter
-        for more details.
-    """
-
-    CATCH_EXCEPTIONS = True
-    """ If True, catch all exceptions and report them at the end.
-
-        Otherwise, the first exception will stop the program with a stack trace
-    """
-
     SKIP_PREFIXES = '__', '.'
     """ Any directory which starts with any prefix in SKIP_PREFIXES and its
         subdirectories are ignored.
     """
 
-    INCLUDE = None
-    """ A list or tuple of regular expressions, or None.
+    WARNINGS_ACTION = 'error'
+    """ What to do if a Python warning occurs.  Possible choices are
+        default, error, ignore, always, module, once
 
-        If non-empty, only modules whose full pathname matches one of these
-        regular expressions will be imported.
-    """
-
-    EXCLUDE = None
-    """ A list or tuple of regular expressions, or None.
-
-        If non-empty, modules whose name matches any of these regular
-        expressions will not be imported.
-    """
-
-    EXPECTED_TO_FAIL = ()
-    """ A list of specific module names that are expected to fail.
-
-        This differs from EXCLUDE because those modules aren't imported at all,
-        but the modules in EXPECTED_TO_FAIL must exist, are imported, and then
-        must fail when imported.
+        `warnings.simplefilter` is set to this value while testing: see
+        https://docs.python.org/3/library/warnings.html#the-warnings-filter
+        for more details.
     """
 
     def __init__(self, *args, **kwds):
         super().__init__(*args, **kwds)
-        self._inc = [re.compile(i) for i in _list(self.INCLUDE)]
-        self._exc = [re.compile(i) for i in _list(self.EXCLUDE)]
+        self._read_env_variables()
+
+        def read_re(s):
+            if s is None:
+                return ()
+            if isinstance(s, str):
+                s = [s]
+            return [re.compile(i.replace('/', r'\.')) for i in s]
+
+        self._exc = read_re(self.EXCLUDE)
+        self._inc = read_re(self.INCLUDE)
 
     def test_all(self):
         successes, failures = self.import_all()
@@ -115,7 +137,9 @@ class TestCase(unittest.TestCase):
 
     def import_all(self):
         successes, failures = [], []
-        paths = _list(self.PROJECT_PATHS or self._guess_paths())
+        paths = self.PROJECT_PATHS
+        paths = [paths] if paths and isinstance(paths, str) else paths
+        paths = _list(paths or self._guess_paths())
 
         warnings.simplefilter(self.WARNINGS_ACTION)
         try:
@@ -185,12 +209,12 @@ class TestCase(unittest.TestCase):
         )
 
     def _read_env_variables(self):
-        for i in set(dir(TestCase)) - set(dir(unittest.TestCase)):
-            if not i.isupper() or i.startwith('_'):
+        for name in set(dir(TestCase)) - set(dir(unittest.TestCase)):
+            if not name.isupper() or name.startswith('_'):
                 continue
 
-            name = ENV_PREFIX + i
-            value = os.environ.get(name)
+            env_name = ENV_PREFIX + name
+            value = os.environ.get(env_name)
             if not value:
                 continue
 
@@ -222,6 +246,27 @@ class TestCase(unittest.TestCase):
         if value.lower() == 'None':
             return None
         return value.split(ENV_SEPARATOR)
+
+
+ENV_PREFIX = '_IMPORT_ALL_'
+ENV_SEPARATOR = ':'
+
+""" ENV_PREFIX is used when setting test attributes using environment
+    variables.  This is convenient for temporarily turning features on or
+    off while debugging.
+
+    For example, to turn off catching exceptions, set the environment variable
+    _IMPORT_ALL_CATCH_EXCEPTIONS=True
+
+    To set a boolean test attribute, use a string starting with t or T for
+    True, or a string starting with f or F for False; any other string gives
+    an error.
+
+    To set a test attribute that's a list of strings, separate those strings
+    with a colon (ENV_SEPARATOR) - for example:
+
+    _IMPORT_ALL_EXCLUDE=my_project.broken:my_project.exper
+"""
 
 
 def split_all(path):
