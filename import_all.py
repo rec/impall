f541f833fbc1c6085cf1c1745300bc1589d3596e
Tom Ritchford <tom@swirly.com>
2019-06-07 12:54:47 +0200

    Much more inline documentation

--- import_all.py
+++ import_all.py
@@ -11,118 +11,127 @@ import warnings
 __author__ = 'Tom Ritchford <tom@swirly.com>'
 __version__ = '0.9.2'
 
-""" A unit test that imports every module in a Python repository.
-
-    It prints all errors and warnings then fails, if there are any.
-    Otherwise prints nothing and suceeds.
+"""A unit test that individually imports every module in a Python repository.
 """
 
 
 class TestCase(unittest.TestCase):
-    """ Import every Python module or file and fail on errors or warnings.
+    """Import every Python module or file and fail on errors or warnings.
 
-        You can customize its behavior by setting one of the following test
-        attributes in one of two ways - you can permanently override that test
-        variable in your own test class, or you can set an environment variable
-        beginning with _IMPORT_ALL to temnporarily override the value.
+    You can customize its behavior by setting one of the following test
+    attributes in one of two ways
 
-        For example, to turn on catching exceptions, either set
-        CATCH_EXCEPTIONS = True in the definition of your TestCase, or
-        set the environment variable _IMPORT_ALL_CATCH_EXCEPTIONS=True
-        before running the tests.
+    * You can permanently override that test variable in your own test
+    class,
+
+    * You can temporarily override the value by setting an environment
+    variable _IMPORT_ALL_<test attribute name>
+
+    For example, to turn on catching exceptions, either set
+    CATCH_EXCEPTIONS = True in the definition of your TestCase, or set
+    the environment variable _IMPORT_ALL_CATCH_EXCEPTIONS=True before
+    running the tests.
+
+    Also, the properties INCLUDE, EXCLUDE, PROJECT_PATH and SKIP_PREFIXES can
+    be lists of strings, or a single string with parts separated by
+    colons.
+
+    And because '.' is a special character in regular expressions, '/' can be
+    used instead of '.' in the INCLUDE and EXCLUDE properties.
+
+    NOTE: to reduce side-effects, `sys.modules` is restored to its
+    original condition after each import, but there might be other
+    side-effects from loading some specific module.  Use the EXCLUDE
+    property to exclude such modules: in general, it is probably a bad
+    idea to have significant side-effects from simply loading a
+    module.
     """
 
     ALL_SUBDIRECTORIES = False
-    """ If True, search all subdirectories.
 
-        If False, stop searching with subdirectories that do not contain
-        an __init__.py file.
+    """If True, search all subdirectories.
 
-        By default, the test attempts to import every Python module and file
-        reachable from its Python root directory.  This means ``import_all``
-        does not load .py files in subdirectories which contain .py files but
-        not a __init__.py file.
+    If False, stop searching with subdirectories that do not contain an
+    __init__.py file.
 
-        This turns out to be what you want most of the time, but if you want
-        import absolutely everything, set the ALL_SUBDIRECTORIES attribute to
-        be True.  If you want to import more specically, you can use the test
-        attributes EXCLUDE, INCLUDE or PROJECT_PATHS.
+    By default, the test attempts to import every Python module and file
+    reachable from its Python root directory.  This means ``import_all``
+    does not load .py files in subdirectories which contain .py files
+    but not a __init__.py file.
+
+    This turns out to be what you want most of the time, but if you want
+    import absolutely everything, set the ALL_SUBDIRECTORIES attribute
+    to be True.  If you want to import more specically, you can use the
+    test attributes EXCLUDE, INCLUDE or PROJECT_PATHS.
     """
 
     CATCH_EXCEPTIONS = False
-    """  If CATCH_EXCEPTIONS is False, the first exception will stop the test
-         entirely and print a stack trace
+    """If CATCH_EXCEPTIONS is False, the first exception will stop the
+    test entirely and print a stack trace
 
-         If True, all exceptions will be caught and reported on at the end.
-         This is most useful when adding this to a new codebase with a lot of
-         import problems.
+    If True, all exceptions will be caught and reported on at the end.
+    This is most useful when adding this to a new codebase with a lot of
+    import problems.
     """
 
     EXCLUDE = None
-    """ A list or tuple of regular expressions, or None.
+    """A list or tuple of regular expressions, or None.
 
-        If non-empty, modules whose name matches any of these regular
-        expressions will not be imported.
+    If non-empty, modules whose name matches any of these regular
+    expressions will not be imported.
 
-        For convenience, since the character '.' is a wildcard in regular
-        expressions, the '/' character can be used in its place, so these
-        two attribute assignments are the same:
+    For convenience, since the character '.' is a wildcard in regular
+    expressions, the '/' character can be used in its place, so these
+    two attribute assignments are the same:
 
-        EXCLUDE = 'foo\\.bar\\.baz:bing\\.bang'
-        EXCLUDE = 'foo/bar/baz:bing/bang'
+      EXCLUDE = 'foo\\.bar\\.baz:bing\\.bang'
+      EXCLUDE = 'foo/bar/baz:bing/bang'
     """
 
     EXPECTED_TO_FAIL = ()
-    """ A list of specific module names that are expected to fail.
+    """A list of specific module names that are expected to fail.
 
-        This differs from EXCLUDE because modules which match EXCLUDE aren't
-        imported at all, but the modules in EXPECTED_TO_FAIL must exist, are
-        imported, and then must fail when imported.
-        """
+    This differs from EXCLUDE because modules which match EXCLUDE aren't
+    imported at all, but the modules in EXPECTED_TO_FAIL must exist, are
+    imported, and then must fail when imported.
+    """
 
     INCLUDE = None
-    """ A list or tuple of regular expressions, or None.
+    """A list or tuple of regular expressions, or None.
 
-        If non-empty, only modules whose full pathname matches one of these
-        regular expressions will be imported.
+    If non-empty, only modules whose full pathname matches one of these
+    regular expressions will be imported.
 
-        Just like in the EXCLUDE attribute, '/' can be used instead of '.'
+    Just like in the EXCLUDE attribute, '/' can be used instead of '.'
     """
 
     PROJECT_PATHS = None
-    """ A list or tuple of path roots that will be recusively loaded.
+    """A list or tuple of path roots that will be recusively loaded.
 
-        If empty, guess PROJECT_PATHS from the root Python directory
-        that contains the definition of the class.
+    If empty, guess PROJECT_PATHS from the root Python directory that
+    contains the definition of the class.
     """
 
     SKIP_PREFIXES = '__', '.'
-    """ Any directory which starts with any prefix in SKIP_PREFIXES and its
-        subdirectories are ignored.
+    """Any directory which starts with a prefix from SKIP_PREFIXES is ignored
     """
 
-    WARNINGS_ACTION = 'error'
-    """ What to do if a Python warning occurs.  Possible choices are
-        default, error, ignore, always, module, once
+    WARNINGS_ACTION = 'default'
+    """A string telling what to do if a Python warning occurs.
 
-        `warnings.simplefilter` is set to this value while testing: see
-        https://docs.python.org/3/library/warnings.html#the-warnings-filter
-        for more details.
+    Possible choices are: default, error, ignore, always, module, once
+
+    `warnings.simplefilter` is set to this value while testing: see
+    https://docs.python.org/3/library/warnings.html#the-warnings-filter
+    for more details.
     """
 
     def __init__(self, *args, **kwds):
         super().__init__(*args, **kwds)
         self._read_env_variables()
 
-        def read_re(s):
-            if s is None:
-                return ()
-            if isinstance(s, str):
-                s = [s]
-            return [re.compile(i.replace('/', r'\.')) for i in s]
-
-        self._exc = read_re(self.EXCLUDE)
-        self._inc = read_re(self.INCLUDE)
+        self._exc = _attribute_to_re(self.EXCLUDE)
+        self._inc = _attribute_to_re(self.INCLUDE)
 
     def test_all(self):
         successes, failures = self.import_all()
@@ -151,9 +160,13 @@ class TestCase(unittest.TestCase):
                         importlib.import_module(module)
                         successes.append(module)
                     except Exception as e:
-                        if not self.CATCH_EXCEPTIONS:
+                        if (
+                            self.CATCH_EXCEPTIONS
+                            or module in self.EXPECTED_TO_FAIL
+                        ):
+                            failures.append((module, e))
+                        else:
                             raise
-                        failures.append((module, e))
                     sys.modules.clear()
                     sys.modules.update(sys_modules)
         finally:
@@ -162,22 +175,22 @@ class TestCase(unittest.TestCase):
 
     def _guess_paths(self):
         sourcefile = inspect.getsourcefile(__class__)
-        path = python_path(os.path.dirname(sourcefile))
+        path = _python_path(os.path.dirname(sourcefile))
 
         for c in os.listdir(path):
-            if has_init_file(c) and not c.startswith('__'):
+            if _has_init_file(c) and not c.startswith('__'):
                 yield c
 
     def _all_imports(self, paths):
         for path in paths:
-            root = python_path(path)
+            root = _python_path(path)
             sys_path = sys.path[:]
             sys.path.insert(0, root)
 
             try:
                 for directory, files in self._walk_code(path):
                     rel = os.path.relpath(directory, root)
-                    module = '.'.join(split_all(rel))
+                    module = '.'.join(_split_all(rel))
                     yield module
 
                     for f in files:
@@ -195,7 +208,7 @@ class TestCase(unittest.TestCase):
             if any(directory.startswith(p) for p in self.SKIP_PREFIXES) or (
                 not self.ALL_SUBDIRECTORIES
                 and directory != path
-                and not has_init_file(directory)
+                and not _has_init_file(directory)
             ):
                 sub_dirs.clear()
             else:
@@ -231,8 +244,6 @@ class TestCase(unittest.TestCase):
         if type(default) is str:
             return value
 
-        # This is hacky and might need to change if more
-        # configuration variables are added.
         if isinstance(default, bool):
             value = value.lower()
             if value.startswith('t'):
@@ -241,10 +252,10 @@ class TestCase(unittest.TestCase):
                 return False
             raise ValueError
 
-        # It's a tuple of strings, split by :s
+        # It's a tuple of strings
         assert type(default) in (type(None), tuple)
-        if value.lower() == 'None':
-            return None
+        if value.lower() == 'None':  # Special case for convenience
+            return ()
         return value.split(ENV_SEPARATOR)
 
 
@@ -269,31 +280,15 @@ ENV_SEPARATOR = ':'
 """
 
 
-def split_all(path):
-    """Use os.path.split repeatedly to split a path into components"""
-    old_path = None
-    components = []
-
-    while path != old_path:
-        (path, tail), old_path = os.path.split(path), path
-        tail and components.insert(0, tail)
-
-    old_path and components.insert(0, old_path)
-    return components
+def _attribute_to_re(s):
+    if s is None:
+        return ()
+    if isinstance(s, str):
+        s = [s]
+    return [re.compile(i.replace('/', r'\.')) for i in s]
 
 
-def python_path(path):
-    """
-    Find the lowest directory in `path` and its parents that does not contain
-    an __init__.py file
-    """
-    while has_init_file(path):
-        path = os.path.dirname(path)
-
-    return path
-
-
-def has_init_file(path):
+def _has_init_file(path):
     """Return True if `path` is a directory containing an __init__.py file"""
     return os.path.exists(os.path.join(path, '__init__.py'))
 
@@ -302,7 +297,18 @@ def _list(s):
     return [s] if isinstance(s, str) else s or []
 
 
-def report(args, file=sys.stdout):
+def _python_path(path):
+    """
+    Find the lowest directory in `path` and its parents that does not contain
+    an __init__.py file
+    """
+    while _has_init_file(path):
+        path = os.path.dirname(path)
+
+    return path
+
+
+def _report(args, file=sys.stdout):
     test_case = TestCase()
     test_case.PROJECT_PATHS = args
     successes, failures = test_case.import_all()
@@ -316,6 +322,19 @@ def report(args, file=sys.stdout):
         print(file=file)
 
 
+def _split_all(path):
+    """Use os.path.split repeatedly to split a path into components"""
+    old_path = None
+    components = []
+
+    while path != old_path:
+        (path, tail), old_path = os.path.split(path), path
+        tail and components.insert(0, tail)
+
+    old_path and components.insert(0, old_path)
+    return components
+
+
 if __name__ == '__main__':
     args = sys.argv[1:] or [os.getcwd()]
-    report(args)
+    _report(args)
