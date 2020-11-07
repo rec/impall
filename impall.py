70d128cafa7fdbc48eb1e1a40da20a7de0fb8965
Tom Ritchford <tom@swirly.com>
2020-11-07 13:27:59 +0100

    Version v1.0.1

--- impall.py
+++ impall.py
@@ -71,7 +71,7 @@ import unittest
 import warnings
 
 __author__ = 'Tom Ritchford <tom@swirly.com>'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __all__ = 'ImpAllTest', 'path_to_import'
 
 EXCLUDE = """
