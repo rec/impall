af7322fcc145fb570d2477c4ba8c34a1d91c81e6
Tom Ritchford <tom@swirly.com>
2021-04-19 14:39:46 +0200

    Remove support for Python 3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import impall
 
 _classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
